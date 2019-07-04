1429a9e22262aff010e156842f1d1a564bc73afc
Andy-Miao <8716649716@qq.com>
2019-07-04 15:39:00 +0800

    HMRuntime

---
+++ HMRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMRuntime
+//
+//  Created by humiao on 2019/7/4.
+//  Copyright © 2019 humiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMRuntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMRuntime
+//
+//  Created by humiao on 2019/7/4.
+//  Copyright © 2019 humiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

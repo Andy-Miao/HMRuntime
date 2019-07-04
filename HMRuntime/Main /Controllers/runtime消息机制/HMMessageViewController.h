20e35c56616b4cf5535e98d31bdb247e7f405a54
Andy-Miao <8716649716@qq.com>
2019-07-04 18:21:24 +0800

    。。

---
+++ HMRuntime/Main /Controllers/runtime消息机制/HMMessageViewController.h
@@ -0,0 +1,18 @@
+//
+//  HMMessageViewController.h
+//  HMRuntime
+//
+//  Created by humiao on 2019/7/4.
+//  Copyright © 2019 humiao. All rights reserved.
+//
+//  Runtime 字典转模型
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMMessageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HMRuntime/Main /Controllers/runtime消息机制/HMMessageViewController.m
@@ -0,0 +1,28 @@
+//
+//  HMMessageViewController.m
+//  HMRuntime
+//
+//  Created by humiao on 2019/7/4.
+//  Copyright © 2019 humiao. All rights reserved.
+//
+
+#import "HMMessageViewController.h"
+
+@interface HMMessageViewController ()
+
+@end
+
+@implementation HMMessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)modelToDic:(id)sender {
+}
+
+@end
